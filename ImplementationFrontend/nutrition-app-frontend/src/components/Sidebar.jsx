e23abd3a35942cfd741495cfa61bd35d4cee562f
Lai Quang Tam <lqtam3004@gmail.com>
2025-12-15 17:44:13 +0700

    Sidebar for user pages

--- ImplementationFrontend/nutrition-app-frontend/src/components/Sidebar.jsx
+++ ImplementationFrontend/nutrition-app-frontend/src/components/Sidebar.jsx
@@ -32,7 +32,7 @@ function Sidebar() {
                             to="/advice"
                             className="flex items-center p-3 rounded-lg text-lg font-medium hover:bg-green-700 transition duration-200"
                         >
-                            <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
+                            <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
                             Tư vấn Dinh dưỡng
                         </Link>
                     </li>
